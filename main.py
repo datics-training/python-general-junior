3d16337df5c7eef9bbb915a09d72af72105b5480
Datmaniac <117644838+datics-training@users.noreply.github.com>
2022-11-07 12:29:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+''' 
+Given three lists of integers: lst1, lst2, lst3, return the sum of integers which are common in all three lists.
+
+sum_common([1, 2, 3], [5, 3, 2], [7, 3, 2]) ➞ 5
+// 2 & 3 are common in all 3 lists.
+
+sum_common([1, 2, 2, 3], [5, 3, 2, 2], [7, 3, 2, 2]) ➞ 7
+// 2, 2 & 3 are common in all 3 lists.
+
+sum_common([1], [1], [2]) ➞ 0
+
+Please write the code to fulfill the requirement and explain your implementation
+'''
+
+
